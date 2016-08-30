84d61328b7b7afd9b474ac5cfac7e0cc483861f6
OlaKlingberg <klingberg2010@gmail.com>
2016-08-30 16:24:18 -0400

    Database configuration
    Configures the db connection.

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+#    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    remoteUrl : 'mongodb://ola2:hem%ligt1349@ec2-54-197-13-189.compute-1.amazonaws.com:27017/dummyDB'
     localUrl: 'mongodb://localhost/meanstacktutorials'
 };
